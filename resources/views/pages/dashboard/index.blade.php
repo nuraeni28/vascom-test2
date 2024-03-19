f2f494d6c9310c471170af2e217c37f9a3066e28
Nuraeni <nuraeniexecutive18@gmail.com>
2024-03-19 16:33:23 +0800

    change navbar

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -1,156 +1,157 @@
 @include('partials.header')
 @include('partials.navbar')
-        <!-- Start right Content here -->
+<!-- Start right Content here -->
 
-       
 
-                <h2>Dashboard</h2>
-                <div class="row">
 
-                    <!-- Column -->
-                    <div class="col-md-6 col-lg-6 col-xl-3">
-                        <div class="card m-b-30">
-                            <div class="card-body">
-                                <div class="d-flex flex-row">
-                                 
-                                    <div class="col-12 align-self-center text-center">
-                                        <div class="m-l-10">
-                                            <p class=" text-muted">Jumlah User</p>
-                                            <h5 class="mt-0 round-inner">{{ $users }}</h5>
-                                            <p class="mb-0 text-muted">User</p>                                                                 
-                                        </div>
-                                    </div>
-                                   
-                                </div>
-                            </div>
+<h2>Dashboard</h2>
+<div class="row">
+
+    <!-- Column -->
+    <div class="col-md-6 col-lg-6 col-xl-3">
+        <div class="card m-b-30">
+            <div class="card-body">
+                <div class="d-flex flex-row">
+
+                    <div class="col-12 align-self-center text-center">
+                        <div class="m-l-10">
+                            <p class=" text-muted">Jumlah User</p>
+                            <h5 class="mt-0 round-inner">{{ $users }}</h5>
+                            <p class="mb-0 text-muted">User</p>
                         </div>
                     </div>
-                    <div class="col-md-6 col-lg-6 col-xl-3">
-                        <div class="card m-b-30">
-                            <div class="card-body">
-                                <div class="d-flex flex-row">
-                                 
-                                    <div class="col-12 align-self-center text-center">
-                                        <div class="m-l-10">
-                                            <p class=" text-muted">Jumlah User Aktif</p>
-                                            <h5 class="mt-0 round-inner">{{ $usersAk }}</h5>
-                                            <p class="mb-0 text-muted">User</p>                                                                 
-                                        </div>
-                                    </div>
-                                   
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-6 col-xl-3">
-                        <div class="card m-b-30">
-                            <div class="card-body">
-                                <div class="d-flex flex-row">
-                                  
-                                    <div class="col-12 align-self-center text-center">
-                                        <div class="m-l-10">
-                                            <p class=" text-muted">Jumlah Produk</p> 
-                                            <h5 class="mt-0 round-inner">{{ $pd }}</h5>
-                                            <p class="mb-0 text-muted">Produk</p>                                                                 
-                                        </div>
-                                    </div>
-                                 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-6 col-xl-3">
-                        <div class="card m-b-30">
-                            <div class="card-body">
-                                <div class="d-flex flex-row">
-                                  
-                                    <div class="col-12 align-self-center text-center">
-                                        <div class="m-l-10">
-                                            <p class=" text-muted">Jumlah Produk Aktif</p> 
-                                            <h5 class="mt-0 round-inner">{{ $pdAk }}</h5>
-                                            <p class="mb-0 text-muted">Produk</p>                                                                 
-                                        </div>
-                                    </div>
-                                 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-12 col-lg-12 col-xl-9">
-                        <div class="card m-b-30">
-                            <div class="card-body">
-                                <div class="d-flex flex-row">
-                                  
-                                    <table id="" class="table table-borderless table-striped">
-                                        <thead>
-                                        <tr>
-                                            <th>No</th>
-                                            <th>Nama</th>
-                                            <th>Harga</th>
-                                            <th>Gambar</th>
-                                     
-                                         
-                                        </tr>
-                                        </thead>
-                                        @php
-                                            $no =1;
-                                        @endphp
-                                        <tbody>
-                                            @foreach ($p as $data)
-                <tr>
-                    <td>{{ $no }}</td>
-                    <td>{{ $data->nama }}</td>
-                    <td>{{ 'Rp ' . number_format($data->harga, 0, ',', '.') }}</td>
-                    <td>
-                        <img src="{{ asset('Foto/' . $data->foto) }}" alt="Gambar produk" width="100">
-                    </td>
-               
-                    
-                    
-                    {{-- <td><img src="{{ asset('Foto/'.$data->foto) }}" style="max-height: 100px;" alt=""></td> --}}
-                   
-                </tr>
-              @php
-                  $no++;
-              @endphp
-            @endforeach
-            
-            
-                                        
-                                    
-                                        </tbody>
-                                    </table>
-                                 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                        </div>          
-                
-                                                               
-                    </div><!-- container -->
-                    
 
-                </div> <!-- Page content Wrapper -->
-
-            </div> <!-- content -->
-
-            <footer class="footer">
-                © 2023 
-            </footer>
-
+                </div>
+            </div>
         </div>
-        <!-- End Right content here -->
-
     </div>
-    <script>
-        $(document).ready(function() {
-            $('#datatable').DataTable({
-                searching: false, // Menonaktifkan fitur pencarian
-                lengthChange: false // Menonaktifkan fitur jumlah entri
-            });
+    <div class="col-md-6 col-lg-6 col-xl-3">
+        <div class="card m-b-30">
+            <div class="card-body">
+                <div class="d-flex flex-row">
+
+                    <div class="col-12 align-self-center text-center">
+                        <div class="m-l-10">
+                            <p class=" text-muted">Jumlah User Aktif</p>
+                            <h5 class="mt-0 round-inner">{{ $usersAk }}</h5>
+                            <p class="mb-0 text-muted">User</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6 col-lg-6 col-xl-3">
+        <div class="card m-b-30">
+            <div class="card-body">
+                <div class="d-flex flex-row">
+
+                    <div class="col-12 align-self-center text-center">
+                        <div class="m-l-10">
+                            <p class=" text-muted">Jumlah Produk</p>
+                            <h5 class="mt-0 round-inner">{{ $pd }}</h5>
+                            <p class="mb-0 text-muted">Produk</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6 col-lg-6 col-xl-3">
+        <div class="card m-b-30">
+            <div class="card-body">
+                <div class="d-flex flex-row">
+
+                    <div class="col-12 align-self-center text-center">
+                        <div class="m-l-10">
+                            <p class=" text-muted">Jumlah Produk Aktif</p>
+                            <h5 class="mt-0 round-inner">{{ $pdAk }}</h5>
+                            <p class="mb-0 text-muted">Produk</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12 col-lg-12 col-xl-9">
+        <div class="card m-b-30">
+            <div class="card-body">
+                <div class="d-flex flex-row">
+
+                    <table id="" class="table table-borderless table-striped">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Harga</th>
+                                <th>Gambar</th>
+
+
+                            </tr>
+                        </thead>
+                        @php
+                            $no = 1;
+                        @endphp
+                        <tbody>
+                            @foreach ($p as $data)
+                                <tr>
+                                    <td>{{ $no }}</td>
+                                    <td>{{ $data->nama }}</td>
+                                    <td>{{ 'Rp ' . number_format($data->harga, 0, ',', '.') }}</td>
+                                    <td>
+                                        <img src="{{ asset('Foto/' . $data->foto) }}" alt="Gambar produk"
+                                            width="100">
+                                    </td>
+
+
+
+                                    {{-- <td><img src="{{ asset('Foto/'.$data->foto) }}" style="max-height: 100px;" alt=""></td> --}}
+
+                                </tr>
+                                @php
+                                    $no++;
+                                @endphp
+                            @endforeach
+
+
+
+
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+</div><!-- container -->
+
+
+</div> <!-- Page content Wrapper -->
+
+</div> <!-- content -->
+
+<footer class="footer">
+    © 2024
+</footer>
+
+</div>
+<!-- End Right content here -->
+
+</div>
+<script>
+    $(document).ready(function() {
+        $('#datatable').DataTable({
+            searching: false, // Menonaktifkan fitur pencarian
+            lengthChange: false // Menonaktifkan fitur jumlah entri
         });
-    </script>
-    <!-- END wrapper -->
+    });
+</script>
+<!-- END wrapper -->
 @include('partials.footer')
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,92 +1,100 @@
 <body class="fixed-left">
- 
+
     <!-- Loader -->
-    <div id="preloader"><div id="status"><div class="spinner"></div></div></div>
+    <div id="preloader">
+        <div id="status">
+            <div class="spinner"></div>
+        </div>
+    </div>
 
     <!-- Begin page -->
-   
 
-        <!-- ========== Left Sidebar Start ========== -->
-        <div class="left side-menu">
-            <button type="button" class="button-menu-mobile button-menu-mobile-topbar open-left waves-effect">
-                <i class="ion-close"></i>
-            </button>
 
-            <!-- LOGO -->
-            <div class="topbar">
-                <div class="text-center">
-                    <a href="{{ route('admin.dashboard') }}" class="logo">Viscomm</a>
-                    <!-- <a href="index.html" class="logo"><img src="assets/images/logo.png" height="24" alt="logo"></a> -->
-                </div>
+    <!-- ========== Left Sidebar Start ========== -->
+    <div class="left side-menu">
+        <button type="button" class="button-menu-mobile button-menu-mobile-topbar open-left waves-effect">
+            <i class="ion-close"></i>
+        </button>
+
+        <!-- LOGO -->
+        <div class="topbar">
+            <div class="text-center">
+                <a href="{{ route('admin.dashboard') }}" class="logo">Vascomm</a>
+                <!-- <a href="index.html" class="logo"><img src="assets/images/logo.png" height="24" alt="logo"></a> -->
             </div>
-            
-            
-            <div class="sidebar-inner slimscrollleft">
-
-                <div id="sidebar-menu">
-                    <ul>
-
-                        <li>
-                            <a href="{{ route('admin.dashboard') }}" class="waves-effect">
-                                <i class="mdi mdi-airplay"></i>
-                                <span> Dashboard </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('admin.user') }}" class="waves-effect">
-                                <i class="mdi mdi-account"></i>
-                                <span> Users</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('admin.produk') }}" class="waves-effect">
-                                <i class="mdi mdi-note"></i>
-                                <span>Produk</span>
-                            </a>
-                        </li>
-                      
-                       
-
-                    </ul>
-                </div>
-                <div class="clearfix"></div>
-            </div> <!-- end sidebarinner -->
         </div>
-        <div class="content-page">
-            <!-- Start content -->
-            <div class="content">
-            <div class="topbar mb-10" >
+
+
+        <div class="sidebar-inner slimscrollleft">
+
+            <div id="sidebar-menu">
+                <ul>
+
+                    <li>
+                        <a href="{{ route('admin.dashboard') }}" class="waves-effect">
+                            <i class="mdi mdi-airplay"></i>
+                            <span> Dashboard </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('admin.user') }}" class="waves-effect">
+                            <i class="mdi mdi-account"></i>
+                            <span> Users</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('admin.produk') }}" class="waves-effect">
+                            <i class="mdi mdi-note"></i>
+                            <span>Produk</span>
+                        </a>
+                    </li>
+
+
+
+                </ul>
+            </div>
+            <div class="clearfix"></div>
+        </div> <!-- end sidebarinner -->
+    </div>
+    <div class="content-page">
+        <!-- Start content -->
+        <div class="content">
+            <div class="topbar mb-10">
 
                 <nav class="navbar-custom" style="background-color: white">
 
-                    <ul class="list-inline float-right mb-0" >
+                    <ul class="list-inline float-right mb-0">
                         <!-- language-->
-                   
+
                         <li class="list-inline-item dropdown notification-list">
-                            <a class="nav-link dropdown-toggle arrow-none waves-effect nav-user" data-toggle="dropdown" href="#" role="button"
-                               aria-haspopup="false" aria-expanded="false"> Halo {{ auth()->user()->name }}
-                                <img src="/Foto/{{ auth()->user()->foto }}" alt="{{ auth()->user()->name }}" class="rounded-circle">
+                            <a class="nav-link dropdown-toggle arrow-none waves-effect nav-user" data-toggle="dropdown"
+                                href="#" role="button" aria-haspopup="false" aria-expanded="false"> Halo
+                                {{ auth()->user()->name }}
+                                <img src="/Foto/{{ auth()->user()->foto }}" alt="{{ auth()->user()->name }}"
+                                    class="rounded-circle">
                             </a>
                             <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
                                 <!-- item-->
                                 <div class="dropdown-item noti-title">
-                                   
-                                    <img src="/Foto/{{ auth()->user()->foto }}" alt="{{ auth()->user()->name }}" class="rounded-circle" height="20">
+
+                                    <img src="/Foto/{{ auth()->user()->foto }}" alt="{{ auth()->user()->name }}"
+                                        class="rounded-circle" height="20">
                                     <h5>{{ auth()->user()->name }}</h5>
                                     <p>{{ auth()->user()->email }}</p>
                                 </div>
-                                                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{ route('logout') }}"><i class="mdi mdi-logout m-r-5 text-muted"></i> Logout</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="{{ route('logout') }}"><i
+                                        class="mdi mdi-logout m-r-5 text-muted"></i> Logout</a>
                             </div>
                         </li>
 
                     </ul>
 
-                  
+
 
                     <div class="clearfix"></div>
 
                 </nav>
 
             </div>
-        <!-- Left Sidebar End -->
+            <!-- Left Sidebar End -->
